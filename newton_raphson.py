5fa836eebb4b78aba665856b0bc1892ad2ce6e5d
EnriqueHavenstein <95871479+EnriqueHavenstein@users.noreply.github.com>
2022-09-29 21:30:50 +0200

    Update newton_raphson.py
    Secant method

--- newton_raphson.py
+++ newton_raphson.py
@@ -12,7 +12,7 @@ def f_prime(x):
 x_prev = float(input('Insert starting point: '))
 programmed_error = float(input('Insert programmed error: '))
 i = 0
-error = 20000
+error = programmed_error + 1
 
 print('i', 'xi', 'f(xi)', "f'(xi)", '|E|')
 print(i, x_prev, f(x_prev), f_prime(x_prev), '-')
@@ -21,6 +21,27 @@ while programmed_error <= error:
     xi = x_prev - (f(x_prev) / f_prime(x_prev))
     f_xi = f(xi)
     f_prime_xi = f_prime(xi)
+    error = abs(xi - x_prev)
+    i += 1
+    x_prev = xi
+    print(i, xi, f(xi), f_prime(xi), error)
+
+# Secant method
+
+def f(x):
+    return (math.e ** (- x)) - x
+
+x_prev = float(input('Insert starting point: '))
+programmed_error = float(input('Insert programmed error: '))
+i = 0
+error = programmed_error + 1
+
+print('i', 'xi', 'f(xi)', '|E|')
+print(i, x_prev, f(x_prev), '-')
+
+while programmed_error <= error:
+    xi = x_prev - ((f(x_prev))*() / f_prime(x_prev))
+    f_xi = f(xi)
     error = abs(xi - x_prev)
     i += 1
     x_prev = xi
