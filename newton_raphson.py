17918ac5ad1f5db52e913065879cd4f41472bba3
EnriqueHavenstein <95871479+EnriqueHavenstein@users.noreply.github.com>
2022-09-29 22:35:50 +0200

    Update newton_raphson.py
    secant method

--- newton_raphson.py
+++ newton_raphson.py
@@ -9,40 +9,46 @@ def f(x):
 def f_prime(x):
     return (-math.e ** (-x)) - 1
 
-x_prev = float(input('Insert starting point: '))
+xi = float(input('Insert starting point: '))
 programmed_error = float(input('Insert programmed error: '))
 i = 0
 error = programmed_error + 1
 
 print('i', 'xi', 'f(xi)', "f'(xi)", '|E|')
-print(i, x_prev, f(x_prev), f_prime(x_prev), '-')
+print(i, xi, f(xi), f_prime(xi), '-')
 
 while programmed_error <= error:
-    xi = x_prev - (f(x_prev) / f_prime(x_prev))
+    x_next = xi - (f(xi) / f_prime(xi))
+    error = abs(x_next - xi)
+    xi = x_next
     f_xi = f(xi)
     f_prime_xi = f_prime(xi)
-    error = abs(xi - x_prev)
     i += 1
-    x_prev = xi
     print(i, xi, f(xi), f_prime(xi), error)
 
 # Secant method
 
+import math
+
 def f(x):
     return (math.e ** (- x)) - x
 
-x_prev = float(input('Insert starting point: '))
+x_prev = float(input('Insert first point: '))
+xi = float(input('Insert second point: '))
 programmed_error = float(input('Insert programmed error: '))
 i = 0
 error = programmed_error + 1
 
 print('i', 'xi', 'f(xi)', '|E|')
 print(i, x_prev, f(x_prev), '-')
+i += 1                                  # for loop to improve this part
+print(i, xi, f(xi), '-')
 
 while programmed_error <= error:
-    xi = x_prev - ((f(x_prev))*() / f_prime(x_prev))
-    f_xi = f(xi)
-    error = abs(xi - x_prev)
-    i += 1
+    x_next = xi - (f(xi) * (x_prev - xi)) / (f(x_prev) - f(xi))
+    error = abs(x_next - xi)
     x_prev = xi
-    print(i, xi, f(xi), f_prime(xi), error)
+    xi = x_next
+    f_xi = f(xi)
+    i += 1
+    print(i, xi, f(xi), error)
