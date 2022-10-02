325410f05bfef526e0c67fd494c50976506baeab
EnriqueHavenstein <95871479+EnriqueHavenstein@users.noreply.github.com>
2022-10-02 19:17:44 +0200

    Update curve_fitting.py
    define main and functions

--- curve_fitting.py
+++ curve_fitting.py
@@ -4,30 +4,48 @@
 
 import numpy as np
 
-n = 7                                               # number of points (xi, yi)
+def linear_least_squares(xi, yi):
+    if len(xi) == len(yi):
+        n = len(xi)                             # number of points (xi, yi)
+        xi_square = []
+        xi_yi = []
+        for i in range(1, n + 1):
+            xi_square.append(xi[i - 1] ** 2)
+            xi_yi.append(xi[i - 1] * yi[i - 1])
+        print('xi: {}'.format(xi))
+        print('yi: {}'.format(yi))
+        print('xi square: {}'.format(xi_square))
+        print('xiyi: {}'.format(xi_yi))
 
-xi = []
-yi = [0.50, 2.50, 2.00, 4.00, 3.50, 6.00, 5.50]
-xi_square = []
-xi_yi = []
+        print('Sum xi = {}'.format(sum(xi)))
+        print('Sum yi = {}'.format(sum(yi)))
+        print('Sum xi square = {}'.format(sum(xi_square)))
+        print('Sum xiyi = {}'.format(sum(xi_yi)))
+
+        a1 = ((n * sum(xi_yi) - sum(xi) * sum(yi)) / (n * sum(xi_square) - (sum(xi)) ** 2))
+        a0 = np.mean(yi) - a1 * np.mean(xi)
+
+        print('a1 = {}'.format(a1))
+        print('a0 = {}'.format(a0))
+
+        print('y = {}x + {}'.format(a1, a0))
+    else:
+        print('Error')
+
+
+# exponential model
+
+# y = Ae ** Bx
+
+n = 5
 
 for i in range(1, n + 1):
     xi.append(i)
     xi_square.append(xi[i-1] ** 2)
     xi_yi.append(xi[i-1] * yi[i-1])
 
-print(xi)
-print(yi)
-print(xi_square)
-print(xi_yi)
 
-print(sum(xi))
-print(sum(yi))
-print(sum(xi_square))
-print(sum(xi_yi))
-
-a1 = ((n * sum(xi_yi) - sum(xi) * sum(yi)) / (n * sum(xi_square) - (sum(xi)) ** 2))
-a0 = np.mean(yi) - a1 * np.mean(xi)
-
-print('a1 = {}'.format(a1))
-print('a0 = {}'.format(a0))
+if __name__ == '__main__':
+    xi = [1, 2, 3, 4, 5, 6, 7]
+    yi = [0.50, 2.50, 2.00, 4.00, 3.50, 6.00, 5.50]
+    linear_least_squares(xi, yi)
